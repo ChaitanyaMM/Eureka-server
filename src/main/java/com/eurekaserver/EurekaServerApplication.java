98f3e0e6cbc85d3cd334f8f5ea3a52d9422afba5
ChaitanyaMM <krishnachaitu.17@gmail.com>
2021-02-07 14:51:10 +0530

    draft eureka server

---
+++ src/main/java/com/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,23 @@
+package com.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+	@GetMapping("/")
+	private String sampleEureka() {
+		
+		return "Eureka-service is up!";
+	}
+}
